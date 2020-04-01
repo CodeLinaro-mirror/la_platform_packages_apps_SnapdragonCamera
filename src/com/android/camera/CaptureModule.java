570dbf1904c149cb1db65f1901c330935f3721df
xianming wang <mingwax@codeaurora.org>
2020-04-01 15:40:20 +0800

    SnapdragonCamera: Close ImageReader after stopBackgroundThread
    Close ImageReader after stoprBackgroundThread.
    
    CRs-Fixed: 2645642
    Change-Id: If1f8658f7f1b7f1676301bcdc71383be844ede10

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -3974,7 +3974,6 @@ public class CaptureModule implements CameraModule, PhotoController,
                                     NamedEntity name = mNamedImages.getNextNameEntity();
                                     String title = (name == null) ? null : name.title;
                                     long date = (name == null) ? -1 : name.date;
-
                                     byte[] bytes = getJpegData(image);
 
                                     Log.i(TAG, "image format:" + image.getFormat() + ",mRawReprocessType:" + mRawReprocessType);
@@ -4605,6 +4604,23 @@ public class CaptureModule implements CameraModule, PhotoController,
         }
     }
 
+    private void closeImageReader() {
+        for (int i = MAX_NUM_CAM - 1; i >= 0; i--) {
+            if (null != mImageReader[i]) {
+                mImageReader[i].close();
+                mImageReader[i] = null;
+            }
+            if (null != mRawImageReader[i]){
+                mRawImageReader[i].close();
+                mRawImageReader[i] = null;
+            }
+        }
+        if (null != mVideoSnapshotImageReader) {
+            mVideoSnapshotImageReader.close();
+            mVideoSnapshotImageReader = null;
+        }
+    }
+
     /**
      * Closes the current {@link CameraDevice}.
      */
@@ -4650,16 +4666,6 @@ public class CaptureModule implements CameraModule, PhotoController,
                     mCameraOpened[i] = false;
                     mCaptureSession[i] = null;
                 }
-
-                if (null != mImageReader[i]) {
-                    mImageReader[i].close();
-                    mImageReader[i] = null;
-                }
-
-                if (null != mRawImageReader[i]){
-                    mRawImageReader[i].close();
-                    mRawImageReader[i] = null;
-                }
             }
 
             closePhysicalImageReaders();
@@ -4670,11 +4676,6 @@ public class CaptureModule implements CameraModule, PhotoController,
             } else {
                 stopCodecThreads();
                 releaseMediaCodec();
-            }
-
-            if (null != mVideoSnapshotImageReader) {
-                mVideoSnapshotImageReader.close();
-                mVideoSnapshotImageReader = null;
             }
         } catch (InterruptedException e) {
             mCameraOpenCloseLock.release();
@@ -5045,6 +5046,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             mActivity.setResultEx(Activity.RESULT_CANCELED, new Intent());
             mActivity.finish();
         }
+        closeImageReader();
         mJpegImageData = null;
     }
 
