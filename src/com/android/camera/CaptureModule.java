66056d1fa35ae569533cc335a3a36ff8d5e02d45
Aoao Yang <aoaoyang@codeaurora.org>
2020-11-05 19:04:33 +0800

    Snapcam: Fix cannot option thumbnail after snapshot with manual focus in promode
    Issue: Cannot option thumbnail after snapshot with manual focus in promode
    Fix: Set the correct completion capture state in promode's manaul_mode
    
    Change-Id: Icf1ea94699956d2d62ce07f0ed6600ad365de540

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -3746,6 +3746,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                     if (mUI.getCurrentProMode() != ProMode.MANUAL_MODE) {
                         unlockFocus(id);
                     } else {
+                        mTakingPicture[id] = false;
                         enableShutterAndVideoOnUiThread(id);
                     }
                     Log.d(TAG,"onShutterButtonRelease");
