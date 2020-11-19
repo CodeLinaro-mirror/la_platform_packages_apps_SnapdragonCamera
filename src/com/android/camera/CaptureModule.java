f37c895ad861706c973855ff026417c493d2ae71
jinwu <jinwu@codeaurora.org>
2020-11-19 20:54:30 +0800

    Snapcam: Fix preview freeze when pause video then flash
    Issue: Preview will freeze when pausing video then click flash
           button.
    Fix: It's caused by camera recovery by video buffer isn't sent
         when pausing video with EIS enabled, so don't use video
         preview request when pausing video.
    
    Change-Id: I60401a534a2c44217d6d3c8ec0173a238d0d136d

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -7516,14 +7516,6 @@ public class CaptureModule implements CameraModule, PhotoController,
                                 mVideoRecordRequestBuilder : mVideoPreviewRequestBuilder),
                                 mCaptureCallback, mCameraHandler);
             } else {
-                if (mRecordingPausing) {
-                    if (mUI.getZoomFixedSupport()) {
-                        applyZoomRatio(mVideoPreviewRequestBuilder, mZoomValue, id);
-                    } else {
-                        applyZoom(mVideoPreviewRequestBuilder, id);
-                    }
-                    captureRequest = mVideoPreviewRequestBuilder.build();
-                }
                 mCurrentSession.setRepeatingRequest(captureRequest, mCaptureCallback,
                         mCameraHandler);
             }
