8c3044e0bf2e3707e3ace423e6b139a8340b9417
jiaoyuan <jiaoyuan@codeaurora.org>
2021-09-02 11:24:04 +0800

    Snapcam: upate raw info ui option
    update raw info ui option when init
    
    Change-Id: I297f9766215a0ed171a2983f39aee09ddc549736

--- src/com/android/camera/SettingsActivity.java
+++ src/com/android/camera/SettingsActivity.java
@@ -1607,6 +1607,7 @@ public class SettingsActivity extends PreferenceActivity {
         updateVideoFlipPreference();
         updatePdnetTogglePreference();
         updateRawFormatPref();
+        updateRawInfoPref();
         updatePictureSizePreferenceButton();
         updateFormatPreference();
     }
