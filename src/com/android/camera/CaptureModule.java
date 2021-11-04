d33e087593401bee9894648c279aaee03fc268eb
Xianming wang <mingwax@codeaurora.org>
2021-11-04 16:49:15 +0800

    SnapCam: StartRecording failed when requestAudio focus failed
      Issues: Request Audio focus failed recording still start.
    Solution: StartRecording failed when requestAudio focus failed.
    
    CRs-Fixed: 3067667
    Change-Id: Ifa6611368cf007004e55d1cd4b12a0fe16dffad5

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -5901,7 +5901,10 @@ public class CaptureModule implements CameraModule, PhotoController,
             return false;
         }
 
-        requestAudioFocus();
+        if (!requestAudioFocus()) {
+            Log.w(TAG, "Audio focus request failed, recording failed");
+            return false;
+        }
         try {
             mMediaRecorder.start(); // Recording is now started
             mMediaRecorderStarted = true;
@@ -8838,15 +8841,18 @@ public class CaptureModule implements CameraModule, PhotoController,
      * Make sure we're not recording music playing in the background, ask the
      * MediaPlaybackService to pause playback.
      */
-    private void requestAudioFocus() {
+    private boolean requestAudioFocus() {
+        boolean requestResult = true;
         AudioManager am = (AudioManager)mActivity.getSystemService(Context.AUDIO_SERVICE);
         // Send request to obtain audio focus. This will stop other
         // music stream.
         int result = am.requestAudioFocus(null, AudioManager.STREAM_MUSIC,
                 AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
         if (result == AudioManager.AUDIOFOCUS_REQUEST_FAILED) {
+            requestResult = false;
             Log.v(TAG, "Audio focus request failed");
         }
+        return requestResult;
     }
 
     private void releaseAudioFocus() {
