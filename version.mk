fbb86e41f881693ec9db2ecb2fb8a6c76db5bca0
jinwu <quic_jinwu@quicinc.com>
2022-03-21 14:04:37 +0800

    Snapcam: Update Version number to 106
    Change-Id: I65bf4cef3819295bd1539177f0c63b813379260b

--- version.mk
+++ version.mk
@@ -39,8 +39,8 @@
 # base_version_minor is 2 digits and auto-increment for developing FR.
 # base_version_build is 3 digits and auto-increment for fixing CR.
 base_version_major := 2
-base_version_minor := 05
-base_version_build := 105
+base_version_minor := 06
+base_version_build := 106
 
 #####################################################
 #####################################################
