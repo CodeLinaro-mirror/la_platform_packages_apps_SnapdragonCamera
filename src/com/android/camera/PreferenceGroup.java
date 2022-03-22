f36da293dfd3268371542edbfe994752ad1f2e3d
jiaoyuan <quic_jiaoyuan@quicinc.com>
2022-03-22 13:56:23 +0800

    Snapcam: fix ConcurrentModificationException
    issue: occur ConcurrentModificationException when stability test
    fix: add synchroniz for array list
    
    Change-Id: Idbe327798bc4ee6db9ee0eccf6c291af28687901

--- src/com/android/camera/PreferenceGroup.java
+++ src/com/android/camera/PreferenceGroup.java
@@ -28,32 +28,42 @@ import java.util.ArrayList;
 public class PreferenceGroup extends CameraPreference {
     private ArrayList<CameraPreference> list =
             new ArrayList<CameraPreference>();
+    private Object lock = new Object();
 
     public PreferenceGroup(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     public void addChild(CameraPreference child) {
-        list.add(child);
+        synchronized (lock) {
+            list.add(child);
+        }
     }
 
     public void removePreference(int index) {
-
-        list.remove(index);
+        synchronized (lock) {
+            list.remove(index);
+        }
     }
 
     public CameraPreference get(int index) {
-        return list.get(index);
+        synchronized (lock) {
+            return list.get(index);
+        }
     }
 
     public int size() {
-        return list.size();
+        synchronized (lock) {
+            return list.size();
+        }
     }
 
     @Override
     public void reloadValue() {
-        for (CameraPreference pref : list) {
-            pref.reloadValue();
+        synchronized (lock) {
+            for (CameraPreference pref : list) {
+                pref.reloadValue();
+            }
         }
     }
 
@@ -65,15 +75,17 @@ public class PreferenceGroup extends CameraPreference {
         // Find a leaf preference with the given key. Currently, the base
         // type of all "leaf" preference is "ListPreference". If we add some
         // other types later, we need to change the code.
-        for (int i = 0; i < list.size(); i++) {
-            CameraPreference pref = list.get(i);
-            if (pref != null && pref instanceof ListPreference) {
-                ListPreference listPref = (ListPreference) pref;
-                if(listPref.getKey().equals(key)) return listPref;
-            } else if(pref != null && pref instanceof PreferenceGroup) {
-                ListPreference listPref =
-                        ((PreferenceGroup) pref).findPreference(key);
-                if (listPref != null) return listPref;
+        synchronized (lock) {
+            for (int i = 0; i < list.size(); i++) {
+                CameraPreference pref = list.get(i);
+                if (pref != null && pref instanceof ListPreference) {
+                    ListPreference listPref = (ListPreference) pref;
+                    if (listPref.getKey().equals(key)) return listPref;
+                } else if (pref != null && pref instanceof PreferenceGroup) {
+                    ListPreference listPref =
+                            ((PreferenceGroup) pref).findPreference(key);
+                    if (listPref != null) return listPref;
+                }
             }
         }
         return null;
