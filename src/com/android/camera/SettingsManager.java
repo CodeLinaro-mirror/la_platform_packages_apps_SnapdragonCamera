53b7c8b8bdee1914d8ad55c83f1cb5eb52de651d
Xianming wang <quic_mingwax@quicinc.com>
2022-04-06 15:37:04 +0800

    SnapCam: Front camera sensor can support HDR
    Front camera sensor can support HDR, does not support Mixed HDR.
    
    Change-Id: I2bb31f0cd44ccf58615de630bd0d03f2ce0180f5

--- src/com/android/camera/SettingsManager.java
+++ src/com/android/camera/SettingsManager.java
@@ -3528,7 +3528,7 @@ public class SettingsManager implements ListMenu.SettingsListener {
                 ret.add("auto");
             }
         }
-        if ((modes != null && modes.length > 0) && !isFacingFront(mCameraId)) {
+        if (modes != null && modes.length > 0) {
             ret.add("manual");
         }
         if (ret.size() == 1) {
