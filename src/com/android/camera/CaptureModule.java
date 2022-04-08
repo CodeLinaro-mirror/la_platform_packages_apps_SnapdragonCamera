1ff129ad4cdede2bad38fbc7a351002af4196eed
chunzhao <quic_chunzhao@quicinc.com>
2022-04-08 17:42:00 +0800

    Snapcam:Add null protection for saving video
    Issue:Can't capture videos from messaging
    Fix:Don't updateVideo when mCurrentVideoValues is null
    
    Change-Id: I9cac0a3055bce5a3ada74094e3b7a796869a361d

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -9745,7 +9745,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                 }
             }
         }
-        if (mVideoFileDescriptor != null && mCurrentVideoUri != null) {
+        if (mVideoFileDescriptor != null && mCurrentVideoUri != null && mCurrentVideoValues != null) {
             long duration = 0L;
             MediaMetadataRetriever retriever = new MediaMetadataRetriever();
 
