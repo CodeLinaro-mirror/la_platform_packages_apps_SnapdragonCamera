4aa4bb1cbcef7fdb74bffce10ac75cb4ce16bec3
xiaofg <quic_xiaofg@quicinc.com>
2022-05-27 18:04:55 +0800

    Snapcam:restore settings NPE
    Change-Id: Iea025113099c3f4e53ee396df185cdb4f5470322

--- src/com/android/camera/SettingsActivity.java
+++ src/com/android/camera/SettingsActivity.java
@@ -143,7 +143,7 @@ public class SettingsActivity extends PreferenceActivity {
                                               String key) {
             Preference p = findPreference(key);
             Log.i(TAG, "onSharedPreferenceChanged key:" + key);
-            if (p == null) return;
+            if (p == null || null == key) return;
             String value;
             if (p instanceof SwitchPreference) {
                 boolean checked = ((SwitchPreference) p).isChecked();
