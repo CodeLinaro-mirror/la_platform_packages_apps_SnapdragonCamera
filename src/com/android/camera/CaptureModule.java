0fcea008fc6a895a1f83f1e66465deae00388961
jiaoyuan <quic_jiaoyuan@quicinc.com>
2022-06-08 14:45:48 +0800

    Snapcam: fix GTS rear camera issue
    after run GTS front camera case, KEY_FRONT_REAR_SWITCHER_VALUE
    is front, default will open front camera, change to rear camera
    
    Change-Id: Iccd2c64a4eab583f3c9e404ce6e423c12e800ab1

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -6602,6 +6602,9 @@ public class CaptureModule implements CameraModule, PhotoController,
         int facingOfIntentExtras = CameraUtil.getFacingOfIntentExtras(mActivity);
         if (facingOfIntentExtras != -1 && !resumeFromRestartAll) {
             mCurrentSceneMode.setSwithCameraId(facingOfIntentExtras);
+        }else if(facingOfIntentExtras == -1  && mIntentMode == INTENT_MODE_STILL_IMAGE_CAMERA){
+            mCurrentSceneMode.setSwithCameraId(facingOfIntentExtras);
+            mSettingsManager.setValue(SettingsManager.KEY_FRONT_REAR_SWITCHER_VALUE, "rear");
         }
         reinit();
         mPaused = false;
