548e22b8fe4a41c2d45402055721a2636c6e55b8
chunzhao <quic_chunzhao@quicinc.com>
2022-06-21 21:05:06 +0800

    Snapcam:Fix HEIC+RAW snapshot issue
    Issue:Snapshot button stucked then crashed when HEIC+RAW
    Fix:Add imageReader when HEIC+RAW
    Change-Id: Ic07baeb265df42fdedc3ee29ba64d14054e55cc6

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -3118,7 +3118,6 @@ public class CaptureModule implements CameraModule, PhotoController,
                         }
                     }
                 }
-
                 if(mChosenImageFormat == ImageFormat.YUV_420_888 || mChosenImageFormat == ImageFormat.PRIVATE) {
                     if (mPostProcessor.isZSLEnabled()) {
                         if (isMultiResolutionImageReaderEnabled()) {
@@ -3272,12 +3271,15 @@ public class CaptureModule implements CameraModule, PhotoController,
             }
         }else if(mSaveRaw){
             int physicalId = mActiveCameraIds.get(0);
+            Log.d(TAG," mActiveCameraIds="+physicalId);
                     for( int i = 0;i < mPhysicalRawId.length;i++){
                         if(Integer.parseInt(mPhysicalRawId[i]) == physicalId ){
                             builder.addTarget(mPhysicalRawReader[i].getSurface());
                             targetCount++;
-                            builder.addTarget(mPhysicalJpegReader[i].getSurface());
-                            targetCount++;
+                            if(mSettingsManager.JPEG_FORMAT == mSettingsManager.getSavePictureFormat()) {
+                                builder.addTarget(mPhysicalJpegReader[i].getSurface());
+                                targetCount++;
+                            }
                             break;
                         }
                     }
@@ -3407,15 +3409,18 @@ public class CaptureModule implements CameraModule, PhotoController,
                 if (!isLogicalId(id)) {
                     configuration.setPhysicalCameraId(id);
                 }
-                setStreamUseCase(Integer.parseInt(id),SCALER_AVAILABLE_STREAM_USE_CASES_FULL_FOV,configuration);
+                setStreamUseCase(Integer.parseInt(id), SCALER_AVAILABLE_STREAM_USE_CASES_FULL_FOV, configuration);
                 outputConfigurations.add(configuration);
-
-                OutputConfiguration configuration_jpeg = new OutputConfiguration(
-                        mPhysicalJpegReader[i].getSurface());
-                configuration_jpeg.setPhysicalCameraId(id);
-                outputConfigurations.add(configuration_jpeg);
-                Log.d(TAG,"add jpeg output format=jpeg physicalId="+id+" size="
-                        +mPhysicalJpegReader[i].getWidth()+"x"+mPhysicalJpegReader[i].getHeight()+",mPhysicalJpegReader[i].getSurface()="+mPhysicalJpegReader[i].getSurface());
+                Log.d(TAG, "add raw output physicalId=" + id + " size="
+                        + mPhysicalRawReader[i].getWidth() + "x" + mPhysicalRawReader[i].getHeight() + ",mPhysicalRawReader[i].getSurface()=" + mPhysicalRawReader[i].getSurface());
+                if (mSettingsManager.JPEG_FORMAT == mSettingsManager.getSavePictureFormat()) {
+                    OutputConfiguration configuration_jpeg = new OutputConfiguration(
+                            mPhysicalJpegReader[i].getSurface());
+                    configuration_jpeg.setPhysicalCameraId(id);
+                    outputConfigurations.add(configuration_jpeg);
+                    Log.d(TAG, "add jpeg output format=jpeg physicalId=" + id + " size="
+                            + mPhysicalJpegReader[i].getWidth() + "x" + mPhysicalJpegReader[i].getHeight() + ",mPhysicalJpegReader[i].getSurface()=" + mPhysicalJpegReader[i].getSurface());
+                }
             }
         }
         return outputConfigurations;
@@ -4466,7 +4471,9 @@ public class CaptureModule implements CameraModule, PhotoController,
                             }
                             if (mSaveRaw && !isPhysicalRaw() ){
                                 addPhysicalCaptureTarget(captureBuilder);
-                                captureBuilder.removeTarget(mImageReader[id].getSurface());
+                                if(mSettingsManager.JPEG_FORMAT == mSettingsManager.getSavePictureFormat()) {
+                                    captureBuilder.removeTarget(mImageReader[id].getSurface());
+                                }
                             } else if (mSaveRaw && isPhysicalRaw()) {
                                 captureBuilder.addTarget(mRawImageReader[id].getSurface());
                                 captureBuilder.addTarget(mImageReader[id].getSurface());
@@ -5511,6 +5518,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                             mMultiResImageReader.setOnImageAvailableListener(listener,
                                     new HandlerExecutor(mImageAvailableHandler));
                         } else {
+
                             mImageReader[i].setOnImageAvailableListener(listener, mImageAvailableHandler);
                         }
                         if (mRawReprocessType != 0){
