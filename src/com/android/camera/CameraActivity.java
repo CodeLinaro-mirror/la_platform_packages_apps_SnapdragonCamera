2597f030f41f5487d6f76faadb692959e896c31a
xiaofg <quic_xiaofg@quicinc.com>
2022-07-13 15:52:40 +0800

    Snapcam: upgrade target sdk 33
    Change-Id: Ib9c8b3619f3d7b71ba4f9869b409dd6a1354c775

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -1843,10 +1843,6 @@ public class CameraActivity extends Activity
         if (checkSelfPermission(Manifest.permission.CAMERA) ==
                         PackageManager.PERMISSION_GRANTED &&
                 checkSelfPermission(Manifest.permission.RECORD_AUDIO) ==
-                        PackageManager.PERMISSION_GRANTED &&
-                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
-                        PackageManager.PERMISSION_GRANTED &&
-                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
                         PackageManager.PERMISSION_GRANTED) {
             hasCriticalPermission = true;
         } else {
