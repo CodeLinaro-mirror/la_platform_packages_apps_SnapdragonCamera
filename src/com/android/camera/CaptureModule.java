ed7766105aad8f175ec51ff744eeb6df291c06f2
jinwu <quic_jinwu@quicinc.com>
2022-10-20 18:25:53 +0800

    Snapcam: Fix overexposure issue of snapshot in dark
    Issue: when snapshot continuously in dark, sometime AE state was
           from last frame so it would cause capture when Flash was
           triggered.
    Fix:   check the result of setting CONTROL_AE_PRECAPTURE_TRIGGER
           first then check AE state.
    
    Change-Id: I0fdfdf814ffe9a3dee5f597824c77291e42c3f1b

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -2350,13 +2350,15 @@ public class CaptureModule implements CameraModule, PhotoController,
                             lockExposure(id);
                         }
                     }
-                } else if (aeState == null ||
-                        aeState == CaptureResult.CONTROL_AE_STATE_INACTIVE ||
+                } else if (aeState == CaptureResult.CONTROL_AE_STATE_INACTIVE ||
                         aeState == CaptureResult.CONTROL_AE_STATE_LOCKED) {
-                    // AE Mode is OFF, the AE state is always CONTROL_AE_STATE_INACTIVE
-                    // then begain capture and ignore lock AE.
-                    checkAfAeStatesAndCapture(id);
-                } else if (mPrecaptureRequestHashCode[id] ==  result.getRequest().hashCode()) {
+                    if ((mPrecaptureRequestHashCode[id] ==  result.getRequest().hashCode()) || (mPrecaptureRequestHashCode[id] == 0)) {
+                        // AE Mode is OFF, the AE state is always CONTROL_AE_STATE_INACTIVE
+                        // then begain capture and ignore lock AE.
+                        checkAfAeStatesAndCapture(id);
+                    }
+                }
+                if (mPrecaptureRequestHashCode[id] ==  result.getRequest().hashCode()) {
                     Log.i(TAG, "AE trigger request result received, but not converged");
                     mPrecaptureRequestHashCode[id] = 0;
                 }
