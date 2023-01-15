6679d05c2d19d548483a056d6fa682d1bd8c5d95
chunzhao <quic_chunzhao@quicinc.com>
2023-01-15 17:45:04 +0800

    Snapcam:Fix pauseVideoRecording issue in monkey test
    Issue:IllegalStateException in MediaRecorder.pause
    Fix:Don't pause video if video is not started
    Change-Id: I6f9ae0775876dd1219c201f184fae19e6670c7a1

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -10519,7 +10519,7 @@ public class CaptureModule implements CameraModule, PhotoController,
     }
 
     public void onButtonPause() {
-        if (!isRecorderReady())
+        if (!isRecorderReady() || !isRecordingVideo())
             return;
         pauseVideoRecording();
     }
