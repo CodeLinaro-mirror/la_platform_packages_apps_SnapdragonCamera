6e5aa2753b17de3c9237a2d7eb4b0aefd93ff8c7
chunzhao <quic_chunzhao@quicinc.com>
2023-03-07 16:12:51 +0800

    Snapcam:Add IS_PENDING for saving video
    Change-Id: Idd019baee51905e57e8aff4d18e46a471544eef5

--- src/com/android/camera/util/ApiHelper.java
+++ src/com/android/camera/util/ApiHelper.java
@@ -83,4 +83,9 @@ public class ApiHelper {
     public static boolean isAndroidQOrHigher(){
         return Build.VERSION.SDK_INT >= 29;
     }
+
+
+    public static boolean isAndroidROrHigher() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.R;
+    }
 }
