efbad0ebe5db751a14b4c467326cb498314dc342
Xianming wang <quic_mingwax@quicinc.com>
2023-04-27 15:36:39 +0800

    SnapCam: Add statsNNSaliencyEnable vendorTag to control StatsNN
    Add statsNNSaliencyEnable vendorTag to control StatsNN option.
    
    Change-Id: I5d25ab507a43e4985ddd42c86b1c34dfa027c03b

--- src/com/android/camera/SettingsActivity.java
+++ src/com/android/camera/SettingsActivity.java
@@ -1434,6 +1434,10 @@ public class SettingsActivity extends PreferenceActivity {
             removePreference(SettingsManager.KEY_TOUCH_TRACK_FOCUS, photoPre);
             removePreference(SettingsManager.KEY_TOUCH_TRACK_FOCUS, videoPre);
         }
+        boolean isStatsNN = mSettingsManager.isStatsNNSupported();
+        if (!isStatsNN) {
+            removePreference(SettingsManager.KEY_STATSNN_CONTROL, photoPre);
+        }
         Preference p = findPreference(SettingsManager.KEY_FD_SETTING);
         if(p != null) {
             fdExpandListView = new FdExpandListView(SettingsActivity.this);
@@ -1538,7 +1542,9 @@ public class SettingsActivity extends PreferenceActivity {
                     videoAddList.add(SettingsManager.KEY_EXTENDED_MAX_ZOOM);
                     videoAddList.add(SettingsManager.KEY_TONE_MAPPING);
                     videoAddList.add(SettingsManager.KEY_SELECT_MODE);
-                    videoAddList.add(SettingsManager.KEY_STATSNN_CONTROL);
+                    if (isStatsNN) {
+                        videoAddList.add(SettingsManager.KEY_STATSNN_CONTROL);
+                    }
                     videoAddList.add(SettingsManager.KEY_PDNET_TOGGLE);
                     videoAddList.add(SettingsManager.KEY_INSENSOR_ZOOM);
                     videoAddList.add(SettingsManager.KEY_STATS_VISUALIZER_ENABLE);
