060abd3c37cf6363cf668c22688ddb0f4ace9d9d
jiaoyuan <quic_jiaoyuan@quicinc.com>
2023-06-05 15:27:31 +0800

    Snapcam: add capture profile for live snapshot
    if live snapshot format is jpeg R, should set capture profile
    
    Change-Id: I5efaffec1ad2c100e1d4453f964ea0512fddbda7

--- src/com/android/camera/SettingsActivity.java
+++ src/com/android/camera/SettingsActivity.java
@@ -1536,6 +1536,7 @@ public class SettingsActivity extends PreferenceActivity {
                         videoAddList.add(SettingsManager.KEY_AI_CAMERA_SNAPSHOT);
                         videoAddList.add(SettingsManager.KEY_PREVIEW_STABILIZATION);
                         videoAddList.add(SettingsManager.KEY_PREVIEW_PROFILE);
+                        videoAddList.add(SettingsManager.KEY_CAPTURE_PROFILE);
                         videoAddList.add(SettingsManager.KEY_SENSOR_MODE_FS2_VALUE);
                         videoAddList.add(SettingsManager.KEY_VIULL);
                     } else {
