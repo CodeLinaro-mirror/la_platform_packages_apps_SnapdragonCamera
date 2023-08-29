019f332cebbbe840651874b9978d01dbd16660f6
xiaofg <quic_xiaofg@quicinc.com>
2023-08-29 14:22:28 +0530

    Snapcam:get action on resume
    Change-Id: I5a58a50a6278c4ffbfbda0c2ef797e3d69d10b18

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -7846,6 +7846,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             mIsCloseCamera = true;
         }
         mSettingsManager.createCaptureModule(this);
+        initModeByIntent();
         // must change cameraId before "mPaused = false;"
         int facingOfIntentExtras = CameraUtil.getFacingOfIntentExtras(mActivity);
         String action = mActivity.getIntent().getAction();
