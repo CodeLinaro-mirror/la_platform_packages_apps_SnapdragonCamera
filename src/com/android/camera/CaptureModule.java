20499d6b5d4274b82515d01576383c3673fade76
jinwu <quic_jinwu@quicinc.com>
2023-10-27 07:21:22 +0530

    Snapcam: control audio related config with property
    Change-Id: I57f131ed4ab6f3fc3cae06ba787e70d8e7639cb0

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -12966,26 +12966,25 @@ public class CaptureModule implements CameraModule, PhotoController,
         // Get HDR ANS mode. (0 off, 1 on)
         int hdrAns = SettingTranslation
                 .getHdrAnsMode(mSettingsManager.getValue(SettingsManager.KEY_HDR_ANS_MODE));
+        if (PersistUtil.needAudioEncoder()) {
+            AudioManager am = (AudioManager) mActivity.getSystemService(Context.AUDIO_SERVICE);
+            setDefaultHDRParameters(am);
 
-        AudioManager am = (AudioManager) mActivity.getSystemService(Context.AUDIO_SERVICE);
-        setDefaultHDRParameters(am);
+            if(audioRecordingMode == SettingTranslation.AudioRecordingModeHDR) {
+                Log.d(TAG, "Enable HDR");
+                am.setParameters("hdr_record_on=true");
+                am.setParameters((hdrWnr == 0) ? "wnr_on=false" : "wnr_on=true");
+                am.setParameters((hdrAns == 0) ? "ans_on=false" : "ans_on=true");
+                am.setParameters("hdr_audio_channel_count=4");
+                am.setParameters("hdr_audio_sampling_rate=48000");
 
-        if(audioRecordingMode == SettingTranslation.AudioRecordingModeHDR) {
-            Log.d(TAG, "Enable HDR");
-            am.setParameters("hdr_record_on=true");
-            am.setParameters((hdrWnr == 0) ? "wnr_on=false" : "wnr_on=true");
-            am.setParameters((hdrAns == 0) ? "ans_on=false" : "ans_on=true");
-            am.setParameters("hdr_audio_channel_count=4");
-            am.setParameters("hdr_audio_sampling_rate=48000");
-        }
-
-        if(audioRecordingMode == SettingTranslation.AudioRecordingModeHDR) {
-            Log.d(TAG, "cameraId " + mSettingsManager.isFacingFront(cameraId) + " mOrientation " + mOrientation);
-            am.setParameters(mSettingsManager.isFacingFront(cameraId) ? "facing=front" : "facing=back");
-            am.setParameters((mOrientation == 90 || mOrientation == 180)
+                Log.d(TAG, "cameraId " + mSettingsManager.isFacingFront(cameraId) + " mOrientation " + mOrientation);
+                am.setParameters(mSettingsManager.isFacingFront(cameraId) ? "facing=front" : "facing=back");
+                am.setParameters((mOrientation == 90 || mOrientation == 180)
                                 ? "inverted=true" : "inverted=false");
-            am.setParameters((mOrientation == 90 || mOrientation == 270)
+                am.setParameters((mOrientation == 90 || mOrientation == 270)
                                 ? "orientation=landscape" : "orientation=portrait");
+            }
         }
 
         //updateHFRSetting();
@@ -16199,11 +16198,11 @@ public class CaptureModule implements CameraModule, PhotoController,
                 }
             }
         }
-
-        AudioManager am = (AudioManager) mActivity.getSystemService(Context.AUDIO_SERVICE);
-        // Set default values for HDR settings
-        setDefaultHDRParameters(am);
-
+        if (PersistUtil.needAudioEncoder()) {
+            AudioManager am = (AudioManager) mActivity.getSystemService(Context.AUDIO_SERVICE);
+            // Set default values for HDR settings
+            setDefaultHDRParameters(am);
+        }
 
     }
 
@@ -16252,6 +16251,7 @@ public class CaptureModule implements CameraModule, PhotoController,
      * MediaPlaybackService to pause playback.
      */
     private void requestAudioFocus() {
+        if (!PersistUtil.needAudioEncoder()) return;
         AudioManager am = (AudioManager)mActivity.getSystemService(Context.AUDIO_SERVICE);
         // Send request to obtain audio focus. This will stop other
         // music stream.
@@ -16263,6 +16263,7 @@ public class CaptureModule implements CameraModule, PhotoController,
     }
 
     private void releaseAudioFocus() {
+        if (!PersistUtil.needAudioEncoder()) return;
         AudioManager am = (AudioManager)mActivity.getSystemService(Context.AUDIO_SERVICE);
         int result = am.abandonAudioFocus(null);
         if (result == AudioManager.AUDIOFOCUS_REQUEST_FAILED) {
