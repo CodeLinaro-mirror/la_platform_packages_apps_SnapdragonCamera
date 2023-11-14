c5a7caf5f782d973efdc09fa8879a8d6dc84793c
jiaoyuan <quic_jiaoyuan@quicinc.com>
2023-11-14 13:26:41 +0530

    Snapcam: enable shutter button after receive image
    if mfnr is enabled, need to wait image receive then release
    shutter button
    
    Change-Id: Ib479411d01f528929e7247440b735fb302cec519
    (cherry picked from commit 96d9cd3dd80a9e0d4d2af7cc98ef2e8e554cbfb7)

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -6765,6 +6765,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                     @Override
                     public void onImageAvailable(ImageReader reader) {
                         Log.d(TAG, "new yuv image from physical camera " + id);
+                        releaseShutterButton();
                         Image image = reader.acquireNextImage();
                         byte[] yuv = getYUVFromImage(image);
                         mNamedImages.nameNewImage(System.currentTimeMillis());
@@ -6802,6 +6803,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                     @Override
                     public void onImageAvailable(ImageReader reader) {
                         Log.d(TAG, "new yuv 10bit image from physical camera "+id);
+                        releaseShutterButton();
                         Image image = reader.acquireNextImage();
                         byte[] yuv = getYUV10BitFromImage(image);
                         mNamedImages.nameNewImage(System.currentTimeMillis());
@@ -6856,6 +6858,18 @@ public class CaptureModule implements CameraModule, PhotoController,
         }
     }
 
+    private void releaseShutterButton(){
+        if (captureWaitImageReceive()) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "image available for cam enable shutter button " );
+                    mUI.enableShutter(true);
+                }
+            });
+        }
+    }
+
     private void setPhysicalImgReader(Size size, String id, int i) {
         if(mSaveRaw)
         mPhysicalRawReader[i] = ImageReader.newInstance(size.getWidth(),
@@ -6865,6 +6879,7 @@ public class CaptureModule implements CameraModule, PhotoController,
         PhysicalImageListener rawListener = new PhysicalImageListener() {
             @Override
             public void onImageAvailable(ImageReader reader) {
+                releaseShutterButton();
                 Image image = reader.acquireNextImage();
                 ByteBuffer buffer = image.getPlanes()[0].getBuffer();
                 byte[] raw = new byte[buffer.remaining()];
@@ -6914,15 +6929,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                     @Override
                     public void onImageAvailable(ImageReader reader) {
                         Log.d(TAG, "new jpeg R image from physical camera " + id);
-                        if (captureWaitImageReceive()) {
-                            mHandler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Log.d(TAG, " PhysicalJpegImgReader image available for cam enable shutter button");
-                                    mUI.enableShutter(true);
-                                }
-                            });
-                        }
+                        releaseShutterButton();
                         Image image = reader.acquireNextImage();
                         mNamedImages.nameNewImage(System.currentTimeMillis());
                         NamedEntity name = mNamedImages.getNextNameEntity();
@@ -6966,15 +6973,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             @Override
             public void onImageAvailable(ImageReader reader) {
                 Log.d(TAG, "new jpeg image from physical camera " + id);
-                if (captureWaitImageReceive()) {
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Log.d(TAG, " PhysicalJpegImgReader image available for cam enable shutter button");
-                            mUI.enableShutter(true);
-                        }
-                    });
-                }
+                releaseShutterButton();
                 Image image = reader.acquireNextImage();
                 mNamedImages.nameNewImage(System.currentTimeMillis());
                 NamedEntity name = mNamedImages.getNextNameEntity();
