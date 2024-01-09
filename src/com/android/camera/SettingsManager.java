ad7bda518a423ffc629e2999fafde12c5afdbf89
jiaoyuan <quic_jiaoyuan@quicinc.com>
2024-01-09 08:42:55 +0530

    Snapcam: add limit for jpeg r format
    only DYNAMIC_RANGE_TEN_BIT is supported will show jpeg R format
    
    Change-Id: I3e301252fb97846dcac3d46c3e71bdb5aebc3631

--- src/com/android/camera/SettingsManager.java
+++ src/com/android/camera/SettingsManager.java
@@ -4386,7 +4386,7 @@ public class SettingsManager implements ListMenu.SettingsListener {
         if (supportHeic == 1){
             ret.add(String.valueOf(SettingsManager.HEIF_FORMAT));
         }
-        if(CaptureModule.CURRENT_MODE != CaptureModule.CameraMode.RTB) {
+        if(CaptureModule.CURRENT_MODE != CaptureModule.CameraMode.RTB && isDynamicRangeTenBitSupported()) {
             ret.add(String.valueOf(SettingsManager.JPEG_R_FORMAT));
         }
         return ret;
