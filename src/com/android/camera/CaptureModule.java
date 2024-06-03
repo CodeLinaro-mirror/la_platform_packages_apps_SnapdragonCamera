e82a1968d1d39926c9d91178e61d152886ee3ce0
chunzhao <quic_chunzhao@quicinc.com>
2024-06-03 16:40:56 +0800

    Snapcam:Fix preview issue in batch mode
    Issue: Preview hung in batch mode
    Fix: Add video surface in batch mode
    Change-Id: I3e9595e19a12f03586749239e9de6ce50ac30852

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -4166,6 +4166,9 @@ public class CaptureModule implements CameraModule, PhotoController,
             }
             mIsPreviewingVideo = true;
             if (isHighSpeedRateCapture()) {
+                if(isBatchMode() && mVideoRecordingSurface != null){
+                    mVideoRecordRequestBuilder.addTarget(mVideoRecordingSurface);
+                }
                 createHighSpeedSession(cameraId);
             } else {
                 createRegularSession(cameraId);
@@ -11909,7 +11912,9 @@ public class CaptureModule implements CameraModule, PhotoController,
                 mVideoRecordRequestBuilder.removeTarget(mPhysicalMediaSurfaces[i]);
             }
         }
-        mVideoRecordRequestBuilder.removeTarget(mVideoRecordingSurface);
+        if(!isBatchMode()) {
+            mVideoRecordRequestBuilder.removeTarget(mVideoRecordingSurface);
+        }
         if (!PersistUtil.enableMediaRecorder()) {
             mFrameProcessor.setVideoOutputSurface(null);
             mFrameProcessor.onClose();
@@ -13718,7 +13723,13 @@ public class CaptureModule implements CameraModule, PhotoController,
             Log.w(TAG,EXCEPTION_LOG,"exception e="+e);
         }
     }
-
+    private boolean isBatchMode(){
+        String buffermode = mSettingsManager.getValue(SettingsManager.KEY_HFR_BUFFER_MODE);
+        if(buffermode != null && buffermode.equals("0")) {
+            return true;
+        }
+        return false;
+    }
     private void applySharpnessControlModes(CaptureRequest.Builder request) {
         String value = mSettingsManager.getValue(SettingsManager.KEY_SHARPNESS_CONTROL_MODE);
         if (value != null) {
