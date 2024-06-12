419caf8ce1b59ed63684740110bae96b07719e01
jiaoyuan <quic_jiaoyuan@quicinc.com>
2024-06-12 12:49:13 +0530

    Snapcam:add perflock for hdr10p video case
    add perflock for hdr10p video case
    
    Change-Id: I4b54c52edb915acdd6d86d816ae9fd1f9b8e062c

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -10999,6 +10999,9 @@ public class CaptureModule implements CameraModule, PhotoController,
             mRecordingStoped = true;
             return false;
         }
+        int[] list = {0x40800000, 0X3AC};
+        if(mPostProcessor.isJniAPISupported() && mSettingsManager.getValue(SettingsManager.KEY_VIDEO_ENCODER_PROFILE).equals("HEVCProfileMain10HDR10Plus"))
+            mPostProcessor.nativePerfLockAcq(2, 0, list, list.length);
         requestAudioFocus();
         if (PersistUtil.enableMediaRecorder()) {
             if (!startMediaRecorder()) {
@@ -12011,6 +12014,8 @@ public class CaptureModule implements CameraModule, PhotoController,
         if(mIntentMode != INTENT_MODE_VIDEO) {
             mStopRecPending = false;
         }
+        if(mPostProcessor.isJniAPISupported() && mSettingsManager.getValue(SettingsManager.KEY_VIDEO_ENCODER_PROFILE).equals("HEVCProfileMain10HDR10Plus"))
+            mPostProcessor.nativePerfLockRelease(2);
     }
 
     private void setVideoFlashOff() {
