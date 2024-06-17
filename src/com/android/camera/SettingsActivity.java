45b88ffefe4d80e101a6169fa8b16514c83b6166
jiaoyuan <quic_jiaoyuan@quicinc.com>
2024-06-17 13:46:21 +0530

    Snapcam: mk file change to bp
    Change-Id: If679f8e89d1b562b17165780093406a44d77c50d

--- src/com/android/camera/SettingsActivity.java
+++ src/com/android/camera/SettingsActivity.java
@@ -2077,7 +2077,9 @@ public class SettingsActivity extends PreferenceActivity {
         try {
             String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
             int index = versionName.indexOf(' ');
-            versionName = versionName.substring(0, index);
+            if(index >= 0) {
+                versionName = versionName.substring(0, index);
+            }
             findPreference("version_info").setSummary(versionName);
         } catch (PackageManager.NameNotFoundException e) {
             Log.w(TAG,e.toString());
