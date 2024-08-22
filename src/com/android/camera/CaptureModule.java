f06ae0a78f551c0cc905c04fe36bbe40b3fdb6d1
jinwu <quic_jinwu@quicinc.com>
2024-08-22 08:29:49 +0530

    Snapcam: Update SDK version for Android U
    Update SDK version to 34.
    
    Change-Id: If24a80820118be18ceb26660e329bbee9a00a359
    (cherry picked from commit d0c843434b9051e8134b3aa4183011f893599c7c)

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -9148,7 +9148,6 @@ public class CaptureModule implements CameraModule, PhotoController,
         mCurrentVideoValues.put(MediaStore.Video.Media.DATE_TAKEN, dateTaken);
         mCurrentVideoValues.put(MediaStore.MediaColumns.DATE_MODIFIED, dateTaken / 1000);
         mCurrentVideoValues.put(MediaStore.Video.Media.MIME_TYPE, mime);
-        mCurrentVideoValues.put(MediaStore.Video.Media.DATA, path);
         mCurrentVideoValues.put(MediaStore.Video.Media.RESOLUTION,
                 "" + mVideoSize.getWidth() + "x" + mVideoSize.getHeight());
         Location loc = mLocationManager.getCurrentLocation();
@@ -9206,7 +9205,6 @@ public class CaptureModule implements CameraModule, PhotoController,
                     contentValues.put(MediaStore.Video.Media.DATE_TAKEN, dateTaken);
                     contentValues.put(MediaStore.MediaColumns.DATE_MODIFIED, dateTaken / 1000);
                     contentValues.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
-                    contentValues.put(MediaStore.Video.Media.DATA, physicalFileName);
                     mActivity.getMediaSaveService().addVideo(physicalFileName,
                             0L, contentValues,
                             mOnVideoSavedListener, mContentResolver);
