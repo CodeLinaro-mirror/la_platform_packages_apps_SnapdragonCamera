924bd6adfbea7358ae5f3fbe51c434d4d466bc76
chunzhao <quic_chunzhao@quicinc.com>
2024-10-29 08:34:09 +0530

    Snapcam:Fix camera mode switch issue
    Issue:camera crash seen when slide zoombar and snapshot
    Fix:Don't do mode switch on zoombar
    
    Change-Id: Idca4f361b8e5431833ead552a6220ca279771180

--- src/com/android/camera/util/CameraUtil.java
+++ src/com/android/camera/util/CameraUtil.java
@@ -193,6 +193,7 @@ public class CameraUtil {
     public static String mWidth,mHeight,mFrameRate,mTitle;
     public static int mFps;
     public static int modeLayHeigh;
+    public static int zoomBarHeigh;
 
     private static long mStartTime;
     private static String mStartStr;
@@ -1852,6 +1853,9 @@ public class CameraUtil {
     }
     public static void setModeLayHeight(int height){
         modeLayHeigh = height;
+    }
+    public static void setZoomBarHeight(int height){
+        zoomBarHeigh = height;
     }
     public static class IntegerLock {
         private int value;
