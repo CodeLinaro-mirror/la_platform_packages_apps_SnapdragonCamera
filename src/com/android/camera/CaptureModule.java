42330917873c234f11a03be6f28c2eca0ff12b1d
jiaoyuan <quic_jiaoyuan@quicinc.com>
2024-11-19 08:33:18 +0530

    Snapcam: limit live snapshot size
    limit live snapshot size to video size when enable any hdr option
    
    Change-Id: Ia99c745766878ec8da022b30999561ef119ae925

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -10401,7 +10401,7 @@ private boolean isDevOptionSetting(){
         mVideoSnapshotSize = getMaxPictureSizeLiveshot(getMainCameraId(),mVideoSize.getWidth(),
                 mVideoSize.getHeight());
         if (getCurrenCameraMode() == CameraMode.CINEMATIC ||
-                (PersistUtil.getModelInfo().contains("7750") && (mSettingsManager.isMfSHDREnable() || mSettingsManager.getVideoHdrMode().equals("auto")))){
+                (PersistUtil.getModelInfo().contains("7750") && (mSettingsManager.isMfSHDREnable() || mSettingsManager.isLimitedHDR() || mSettingsManager.isSHDRLimited()))){
             mVideoSnapshotSize = mVideoSize;
         }
         String mlVideo = mSettingsManager.getValue(SettingsManager.KEY_ML_VIDEO);
