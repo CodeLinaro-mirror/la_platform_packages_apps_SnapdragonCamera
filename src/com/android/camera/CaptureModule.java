56228767e64c9e35692ad400a11ade533b7444a7
chunzhao <quic_chunzhao@quicinc.com>
2025-02-21 11:11:20 +0530

    Snapcam:Fix ev bar showing issue when taf in front camera
    Change-Id: I8d24ce190f97f6c5667c40dbe9c6de37edc12bf3

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -9429,6 +9429,7 @@ private boolean isDevOptionSetting(){
         }
 
         mUI.setFocusPosition(x, y);
+        mUI.showEvSeekbar(x,y);
         int x_ = newXY[0];
         int y_ = newXY[1];
         mUI.setFocusPointInPreview(x_, y_);
@@ -9468,6 +9469,7 @@ private boolean isDevOptionSetting(){
             mLockAFAE = LOCK_AF_AE_STATE_START;
             applyIsAfLock(true);
             mUI.setFocusPosition(x, y);
+            mUI.showEvSeekbar(x,y);
             x = newXY[0];
             y = newXY[1];
             mUI.onFocusStarted();
