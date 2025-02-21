62485417449e1e0d3cf0a76c7235f577af9ae5bc
chunzhao <quic_chunzhao@quicinc.com>
2025-02-21 12:51:23 +0530

    Snapcam: disable Zoom when MVHEVC enabled
    Change-Id: If6d4f723cfba2e26928c5ed8b865d91c38258502

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -17526,10 +17526,10 @@ private boolean isDevOptionSetting(){
             mUI.hideZoomSeekBar();
         } else if(multiCam != null && multiCam.equals("on")) {
             mUI.hideZoomSeekBar();
-        }else{
+        }else {
             mUI.showZoomSeekBar();
-            mUI.enableZoomSeekBar(true);
         }
+
     }
 
     private void updateAICameraSeekBar(){
