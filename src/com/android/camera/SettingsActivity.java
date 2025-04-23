2e4df101a841568fb8e7d455245d905da2c91bdd
jinwu <quic_jinwu@quicinc.com>
2025-04-23 13:13:21 +0530

    Snapcam: Add Exposure Metering to Video/HFR
    Tuning team requests to have this option in video and HFR mode
    for tuning purpose.
    
    Change-Id: I41d1d930c34f0d0aa9681e5a5dc2138c35439d83

--- src/com/android/camera/SettingsActivity.java
+++ src/com/android/camera/SettingsActivity.java
@@ -1688,6 +1688,7 @@ public class SettingsActivity extends PreferenceActivity {
                     videoAddList.add(SettingsManager.KEY_SWITCH_CAMERA);
                     videoAddList.addAll(videoOnlyList);
                     videoAddList.add(SettingsManager.KEY_ANTI_BANDING_LEVEL);
+                    videoAddList.add(SettingsManager.KEY_EXPOSURE_METERING_MODE);
                     if (mode == VIDEO) {
                         videoAddList.add(SettingsManager.KEY_FD_SMILE);
                         videoAddList.add(SettingsManager.KEY_FD_GAZE);
