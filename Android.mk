de57a232b3834fc0fd43f489d9f3a1bd41e2e020
Jaimin Thakkar <jaimthak@qti.qualcomm.com>
2025-10-03 18:54:08 +0530

    SnapdragionCamera: Enable libraries for mfnr
    Enable libraries for mfnr
    	- libjni_mfnrutil
    	- libjni_aidenoiserutilv2
    	- libjni_imageutil
    
    CRs-Fixed: 4298753
    Change-Id: Ief54126d58b3c4b28d06a0bcd7af1e81490daf99

--- Android.mk
+++ Android.mk
@@ -44,11 +44,11 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 # the libraries in the APK, otherwise just put them in /system/lib and
 # leave them out of the APK
 
-#ifneq (,$(TARGET_BUILD_APPS))
-#  LOCAL_JNI_SHARED_LIBRARIES := libjni_mfnrutil libjni_aidenoiserutilv2 libjni_imageutil
-#else
-#  LOCAL_REQUIRED_MODULES := libjni_mfnrutil libjni_aidenoiserutilv2 libjni_imageutil
-#endif
+ifneq (,$(TARGET_BUILD_APPS))
+  LOCAL_JNI_SHARED_LIBRARIES := libjni_mfnrutil libjni_aidenoiserutilv2 libjni_imageutil
+else
+  LOCAL_REQUIRED_MODULES := libjni_mfnrutil libjni_aidenoiserutilv2 libjni_imageutil
+endif
 
 include $(BUILD_PACKAGE)
 
