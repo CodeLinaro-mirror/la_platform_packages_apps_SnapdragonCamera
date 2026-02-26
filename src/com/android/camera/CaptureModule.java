e403bce883b4cfbd7df164b5bf2f14b01d3c3707
jiaoyuan <jiaoyuan@qti.qualcomm.com>
2026-02-26 14:25:35 +0800

    Snapcam: enable motion photo
    fix motion photo enable/disable issue
    
    Change-Id: Idf8fad721e35ca63086f0fd39040a85bfbb2674a

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -2469,6 +2469,7 @@ public class CaptureModule implements CameraModule, PhotoController,
         List<Surface> list = new LinkedList<Surface>();
         mState[id] = STATE_PREVIEW;
         mControlAFMode = CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE;
+        mPreviewOutputConfiguration = null;
         try {
             // We set up a CaptureRequest.Builder with the output Surface.
             mPreviewRequestBuilder[id] = getRequestBuilder(id, 0);
@@ -2486,13 +2487,17 @@ public class CaptureModule implements CameraModule, PhotoController,
                             Log.i(TAG, "capturesession - onConfigured "+ id);
                             mCurrentSessionClosed = false;
                             if(mPreviewOutputConfiguration != null) {
-                                mPreviewOutputConfiguration.addSurface(getPreviewSurfaceForSession(id));
-                                try {
-                                    List<OutputConfiguration> finalizeOutputConfigs = new ArrayList<>();
-                                    finalizeOutputConfigs.add(mPreviewOutputConfiguration);
-                                    cameraCaptureSession.finalizeOutputConfigurations(finalizeOutputConfigs);
-                                } catch (Exception e) {
-                                    Log.e(TAG, "finalizeOutputConfigurations with exception:" + e.toString());
+                                Surface previewSur = getPreviewSurfaceForSession(id);
+                                waitForPreviewSurfaceReady();
+                                if (mSurfaceReady && previewSur.isValid()) {
+                                    mPreviewOutputConfiguration.addSurface(previewSur);
+                                    try {
+                                        List<OutputConfiguration> finalizeOutputConfigs = new ArrayList<>();
+                                        finalizeOutputConfigs.add(mPreviewOutputConfiguration);
+                                        cameraCaptureSession.finalizeOutputConfigurations(finalizeOutputConfigs);
+                                    } catch (Exception e) {
+                                        Log.e(TAG, "finalizeOutputConfigurations with exception:" + e.toString());
+                                    }
                                 }
                             }
                             if(isLivePhotoOn()){
@@ -2720,7 +2725,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                         OutputConfiguration videoConfig = new OutputConfiguration(mVideoImageReader.getSurface());
                         videoConfig.enableSurfaceSharing();
                         list.add(mVideoImageReader.getSurface());
-                        mPreviewRequestBuilder[BAYER_ID].addTarget(mVideoImageReader.getSurface());
+                        mPreviewRequestBuilder[id].addTarget(mVideoImageReader.getSurface());
                     }
                     for (Surface s : list) {
                         if (s == surface) {
