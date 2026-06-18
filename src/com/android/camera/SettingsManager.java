db63f1b625eae5514209ce2a7280ea895cacd108
Sudheer Kanchumurthi <c_skanch@qti.qualcomm.com>
2026-06-18 03:58:03 -0700

    Revert "Snapcam: disable eis in HFR mode"
    This reverts commit 61b66c42a28aa6d7727c43b8b52d8eee5f35a75f.
    
    Reason for revert: revert as confirmed by venky
    
    Change-Id: I72918f855067ca61d977bf41d59c1829fb6e9ec6

--- src/com/android/camera/SettingsManager.java
+++ src/com/android/camera/SettingsManager.java
@@ -1071,8 +1071,7 @@ public class SettingsManager implements ListMenu.SettingsListener {
     }
 
     public boolean isVideoEisSupported(){
-        if((PersistUtil.getModelInfo().contains("6850") || PersistUtil.getModelInfo().contains("4875")) &&
-                mCaptureModule.getCurrenCameraMode() == CaptureModule.CameraMode.HFR){
+        if(PersistUtil.getModelInfo().contains("6850") && mCaptureModule.getCurrenCameraMode() == CaptureModule.CameraMode.HFR){
             return false;
         }else{
             return true;
@@ -3011,7 +3010,7 @@ public class SettingsManager implements ListMenu.SettingsListener {
 
     public boolean isFrontIDHFRSupported() {
         boolean result = true;
-        if (-1 == CaptureModule.FRONT_ID || PersistUtil.getModelInfo().contains("6850") || PersistUtil.getModelInfo().contains("4875")) {
+        if (-1 == CaptureModule.FRONT_ID || PersistUtil.getModelInfo().contains("6850")) {
             result = false;
         } else {
             ListPreference videoQuality = mPreferenceGroup.findPreference(KEY_VIDEO_QUALITY);
@@ -4735,14 +4734,7 @@ public class SettingsManager implements ListMenu.SettingsListener {
                 }
             }
             if (range == null) {
-                for (Capability cap : extendedSceneModeCaps) {
-                    if (CaptureModule.CURRENT_MODE == CaptureModule.CameraMode.RTB && cap.getMode() == CameraMetadata.CONTROL_EXTENDED_SCENE_MODE_BOKEH_CONTINUOUS) {
-                        range = cap.getZoomRatioRange();
-                    }
-                }
-                if(range == null){
-                    return null;
-                }
+                return null;
             }
             result[0] = range.getLower();
             result[1] = range.getUpper();
